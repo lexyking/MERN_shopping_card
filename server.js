e14e8d8f02164d4d6c68d82aaade7e6f14a49051
lexyking <sahiealexandre@hotmail.fr>
2019-08-24 18:41:02 +0100

    server created and linked to the db

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+const app = express();
+dotenv.config();
+
+
+//BodyParser and morgan Middlewares
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+//Connecting to MongoDB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  },
+
+  () => {
+    console.log("connected to db!");
+  })
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server listening on mode ${process.env.PORT} or at local ${port}`)
+)
