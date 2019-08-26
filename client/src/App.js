7f249025bcaed2430b5f8a03ffbaa419d7b28ce7
lexyking <sahiealexandre@hotmail.fr>
2019-08-26 16:11:14 +0100

    added the store file and the provide within app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import AppNavbar from './components/header/AppNavBar';
 import ShoppingList from './components/main/ShoppingList';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <section className="App">
-      <AppNavbar />
-      <ShoppingList />
-    </section>
+    <Provider store={store}>
+      <section className="App">
+        <AppNavbar />
+        <ShoppingList />
+      </section>
+    </Provider>
   );
 }
 
