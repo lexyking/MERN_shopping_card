d3dc1e80810728eacc0481c7f3f930405863fb31
lexyking <sahiealexandre@hotmail.fr>
2019-08-26 17:31:15 +0100

    redux store ready: getting adding and deleting items

--- client/src/components/itemModal.js
+++ client/src/components/itemModal.js
@@ -11,9 +11,87 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
+import uuid from 'uuid';
+import itemReducer from '../reducers/itemReducer';
 
 class ItemModal extends Component {
+  state = {
+    modal: false,
+    name: ''
+  }
 
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    })
+  }
+
+  onChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const newItem = {
+      id: uuid(),
+      name: this.state.name
+    }
+
+    // Add item via addItem action
+    this.props.addItem(newItem);
+
+    //Close the modal
+    this.toggle();
+  }
+
+  render() {
+    return (
+      <section>
+        <Button
+          color="dark"
+          style={{ marginBottom: '2rem' }}
+          onClick={this.toggle}
+        >Add Item</Button>
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+        >
+          <ModalHeader
+            toggle={this.toggle}
+          >
+            Add To Shopping List
+          </ModalHeader>
+
+          <ModalBody>
+            <Form onSubmit={this.onSubmit}>
+              <FormGroup>
+                <Label for="item">Item</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  id="item"
+                  placeholder="Add shopping Item"
+                  onChange={this.onChange}
+                  value={this.state.name}
+                >
+                </Input>
+                <Button
+                  color="dark"
+                  style={{ marginTop: "2rem" }}
+                  block
+                >Submit</Button>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </section>
+    );
+  }
 }
 
-export default connect()(ItemModal);
+const mapStateToProps = state => ({
+  item: state.item
+})
+
+export default connect(mapStateToProps, { addItem })(ItemModal);
