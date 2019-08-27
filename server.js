506fb868e8fcca72a89b000396dd7b29a199f2b9
lexyking <sahiealexandre@hotmail.fr>
2019-08-27 23:31:27 +0100

    create the build within nodejs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -28,6 +29,16 @@ mongoose.connect(
 
 //Routes
 app.use('/api/items', items)
+
+//Server static assets id in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 
 const port = process.env.PORT || 5000;
