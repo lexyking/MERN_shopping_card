b6a63b434ad0ca375385beff561d501633b4a092
lexyking <sahiealexandre@hotmail.fr>
2019-08-27 23:24:59 +0100

    heroku deploiment

--- client/src/components/itemModal.js
+++ client/src/components/itemModal.js
@@ -11,7 +11,6 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
-import itemReducer from '../reducers/itemReducer';
 
 class ItemModal extends Component {
   state = {
