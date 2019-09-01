c2a6227d40ee4c906c30667010861cfaff696b31
lexyking <sahiealexandre@hotmail.fr>
2019-09-01 23:08:16 +0100

    set the token from the localstorage

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import { Container } from 'reactstrap';
 
 import { Provider } from 'react-redux';
 import store from './store';
-import { loadUser } from './actions/authActions'
+import { loadUser } from './actions/authActions';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
