406871f5b943fc0749a952074469270ae43a2312
lexyking <sahiealexandre@hotmail.fr>
2019-09-08 12:49:05 +0100

    created authaction file

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -32,6 +32,32 @@ export const loadUser = () => (dispatch, getState) => {
     })
 }
 
+//Register User
+export const register = ({ name, email, password }) => dispatch => {
+  //Headers
+  const config = {
+    header: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  //Request body
+  const body = JSON.stringify({ name, email, password })
+
+  axios.post('api/users', body, config)
+    .then(res => dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data
+    }))
+    .catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status, 'REGISTER_FAIL'))
+      dispatch({
+        type: REGISTER_FAIL
+
+      })
+    })
+}
+
 //setup config/headers
 export const tokenConfig = getState => {
   //Get token from local storage
--- client/src/components/auth/RegistrationModal.js
+++ client/src/components/auth/RegistrationModal.js
@@ -12,6 +12,7 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { register } from '../../actions/authActions';
 
 class RegisterModal extends Component {
   state = {
@@ -24,7 +25,8 @@ class RegisterModal extends Component {
 
   static propTypes = {
     isAuthenticated: PropTypes.bool,
-    error: PropTypes.object.isRequired
+    error: PropTypes.object.isRequired,
+    register: PropTypes.func.isRequired
   }
 
   toggle = () => {
@@ -110,4 +112,7 @@ const mapStateToProps = state => ({
   error: state.error
 })
 
-export default connect(mapStateToProps, {})(RegisterModal);
+export default connect(
+  mapStateToProps,
+  { register }
+)(RegisterModal);
